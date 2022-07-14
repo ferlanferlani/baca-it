b581f86f5d03aad6768dd70bee250e8ccc2aa10a
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-07-14 22:16:27 +0700

    merubah logic file upload dan database

--- database/baca-it.sql
+++ database/baca-it.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2022 at 03:56 PM
+-- Generation Time: Jul 14, 2022 at 05:15 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -31,15 +31,17 @@ CREATE TABLE `ebook` (
   `id` int(11) NOT NULL,
   `judul` varchar(99) NOT NULL,
   `deskripsi` varchar(999) NOT NULL,
-  `pdf` varchar(999) NOT NULL
+  `pdf` varchar(999) NOT NULL,
+  `cover` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `ebook`
 --
 
-INSERT INTO `ebook` (`id`, `judul`, `deskripsi`, `pdf`) VALUES
-(3, 'Mengenal javascript', 'ebook belajar bahasa pemerogramman javascript', 'Mengenaljavascript.pdf');
+INSERT INTO `ebook` (`id`, `judul`, `deskripsi`, `pdf`, `cover`) VALUES
+(3, 'Mengenal javascript', 'ebook belajar bahasa pemerogramman javascript', 'Mengenaljavascript.pdf', 'js.png'),
+(8, 'ebook panduan bootstrap', 'ebook panduan bootstrap', '62d0319c14103.pdf', '62d0319c144a5.png');
 
 --
 -- Indexes for dumped tables
@@ -59,7 +61,7 @@ ALTER TABLE `ebook`
 -- AUTO_INCREMENT for table `ebook`
 --
 ALTER TABLE `ebook`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
